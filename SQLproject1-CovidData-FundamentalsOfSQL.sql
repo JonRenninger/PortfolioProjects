355f63190942bd22d519a1ae78353020068786da
JonRenninger <jon.renninger@gmail.com>
2023-11-20 14:40:58 -0500

    cleaned up comments to make the code more readable

--- SQLproject1-CovidData-FundamentalsOfSQL.sql
+++ SQLproject1-CovidData-FundamentalsOfSQL.sql
@@ -1,4 +1,6 @@
-
+-----------------------------------------------------------------------------------------------------------
+-- base queries
+-----------------------------------------------------------------------------------------------------------
 
 select *
 from PortfolioProject..CovidDeaths
@@ -11,9 +13,10 @@ select location, date, total_cases, new_cases, total_deaths, population
 from PortfolioProject..CovidDeaths
 order by 1,2
 
-
+-----------------------------------------------------------------------------------------------------------
 -- Looking at Total Cases vs Total Deaths
 -- Shows likelihood of dying if you contract covid in your country
+-----------------------------------------------------------------------------------------------------------
 select location, date, total_cases, total_deaths,
 case
 	when total_cases = 0 then NULL
@@ -24,17 +27,19 @@ where location like '%states%'
 order by 1,2
 
 
-
+-----------------------------------------------------------------------------------------------------------
 -- Looking at Total Cases vs Population
 -- Shows what percentage of population got Covid
+-----------------------------------------------------------------------------------------------------------
 select location, date, population, total_cases, (total_cases/population)*100 as PercentPopulationInfected
 from PortfolioProject..CovidDeaths
 where location = 'United States'
 order by 1,2
 
 
-
+-----------------------------------------------------------------------------------------------------------
 -- Looking at Countries with Highest Infection Rate compared to Population
+-----------------------------------------------------------------------------------------------------------
 select location, population, max(total_cases) as HighestInfectionCount, Max((total_cases/population))*100 as PercentPopulationInfected
 from PortfolioProject..CovidDeaths
 --where location = 'United States'
@@ -42,8 +47,9 @@ group by location, population
 order by PercentPopulationInfected desc
 
 
-
+-----------------------------------------------------------------------------------------------------------
 -- Showing Countries with Highest Death Count per Population
+-----------------------------------------------------------------------------------------------------------
 select location, max(cast(Total_deaths as int)) as TotalDeathCount
 from PortfolioProject..CovidDeaths
 where continent is not null and continent != ''
@@ -51,9 +57,10 @@ group by location
 order by TotalDeathCount desc
 
 
-
+-----------------------------------------------------------------------------------------------------------
 -- LET'S BREAK THINGS DOWN BY CONTINENT
 -- Showing continents with the highest death count per population
+-----------------------------------------------------------------------------------------------------------
 select continent, max(cast(Total_deaths as int)) as TotalDeathCount
 from PortfolioProject..CovidDeaths
 where continent is not null and continent != ''
@@ -63,8 +70,9 @@ order by TotalDeathCount desc
 
 
 
-
+-----------------------------------------------------------------------------------------------------------
 -- GLOBAL NUMBERS by each date
+-----------------------------------------------------------------------------------------------------------
 select date, sum(new_cases) as total_cases, sum(cast(new_deaths as int)) as total_deaths,
 case
 	when sum(new_cases) = 0 then NULL
@@ -77,8 +85,9 @@ order by 1,2
 
 
 
-
+-----------------------------------------------------------------------------------------------------------
 -- GLOBAL NUMBERS overall
+-----------------------------------------------------------------------------------------------------------
 drop view if exists GlobalDeathPercentage
 GO
 Create View GlobalDeathPercentage as
@@ -95,8 +104,9 @@ from GlobalDeathPercentage
 
 
 
-
+-----------------------------------------------------------------------------------------------------------
 -- Looking at total population vs vaccinations
+-----------------------------------------------------------------------------------------------------------
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , sum(vac.new_vaccinations) over (partition by dea.location order by dea.date) as RollingPeopleVaccinated
 --, (RollingPeopleVaccinated/population)*100
@@ -109,8 +119,9 @@ order by 2,3
 
 
 
-
+-----------------------------------------------------------------------------------------------------------
 -- USE CTE
+-----------------------------------------------------------------------------------------------------------
 With PopvsVac (Continent, Location, Date, Population, new_vaccinations, RollingPeopleVaccinated)
 as
 (
@@ -131,9 +142,9 @@ from PopvsVac
 
 
 
-
+-----------------------------------------------------------------------------------------------------------
 -- TEMP TABLE
-
+-----------------------------------------------------------------------------------------------------------
 drop table if exists #PercentPopulationVaccinated
 create table #PercentPopulationVaccinated
 (
@@ -164,8 +175,9 @@ from #PercentPopulationVaccinated
 
 
 
-
+-----------------------------------------------------------------------------------------------------------
 -- Creating View to store data for later visualizations
+-----------------------------------------------------------------------------------------------------------
 drop view if exists PercentPopulationVaccinated
 GO
 Create View PercentPopulationVaccinated as
